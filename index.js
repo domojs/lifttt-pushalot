dd36b80321a2889227ea3714b8b9329da3c7edcb
npenin <nicolas.penin@dragon-angel.fr>
2016-01-25 20:13:17 +0100

    fixes

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ module.exports={
                 { name:"Body", displayName:"Body"},
             ],
             delegate:function(fields){
+                    console.log('puhsalot1');
+                    console.log(fields);
                 var result=function(fields){
-                    $.ajax({type:'post', dataType:'json', data:fields, url:"https://pushalot.com/api/sendmessage", success:function(result){
+                    console.log('puhsalot2');
+                    console.log(fields);
+                    $.ajax({type:'post', contentType:'application/json', dataType:'json', data:fields, url:"https://pushalot.com/api/sendmessage", success:function(result){
                         console.log(result.Description);
                     },error:function(){
-                        console.log(arguments);
+                        console.log(arguments[0]);
                     }});
                 };
                 result.fields=fields;
---
+++ test.js
@@ -0,0 +1,4 @@
+require('jnode/setup.js');
+var pushalot=require('./index.js');
+pushalot.actions[0].delegate()({ AuthorizationToken: '9c67b27b8547433194b9a9db96d6c548',
+  Body: 'Un mouvement a été détecté' });
